46b5bc964ef2cf1d4f8d1b70f76aaf2907cc45a1
c4ir0 <108268587+c4ir0@users.noreply.github.com>
2022-10-10 21:38:45 +0200

    Update main.js

--- secrets_js/main.js
+++ secrets_js/main.js
@@ -1,4 +1,7 @@
+api_key
+apikey
 var x = 'hellothisseemstoobeanAPIkeybutTHEvarISNOTnammedLIKEso';
 const y = "it'sverystrongpassword";
 let z = 'password@password';
 let xyz = "sdsa,8272^&%^&@BJ(*NLK";
+api-key
